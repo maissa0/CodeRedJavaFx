d787ebe319c1aa45afcbfc6d0d0c162db9764887
bourgui123 <elyess.borji@esprit.tn>
2024-05-06 10:23:19 +0100

    footer

--- src/main/java/controllers/frontbar.java
+++ src/main/java/controllers/frontbar.java
@@ -1,4 +1,32 @@
 package controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class frontbar {
+
+    @FXML
+    void objectiffront(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ADDCRUD.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Erreur");
+            alert.setHeaderText("Erreur lors de l'ouverture de la fenêtre");
+            alert.setContentText("Une erreur s'est produite lors de l'ouverture de la fenêtre. Veuillez réessayer.");
+            alert.showAndWait();
+        }
+    }
 }
